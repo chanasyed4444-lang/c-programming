db992c60fbbec006c8fdd015f0897e7a7c35e9db
chanasyed4444-lang <chanasyed4444@gmail.com>
2025-11-07 22:39:13 +0530

    Add files via upload

---
+++ prime or not.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int i,n,count=0;
+	printf("enter n value");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		if(n%i==0)
+		count +1;
+	}
+			if (count==2)
+		printf("it is a prime");
+		else
+		{
+			printf("not a prime");
+		}
+		return 0;
+	}
+	
+
