d6f2ecf3249dd83d6f14d37abe8037a0deafe972
chanasyed4444-lang <chanasyed4444@gmail.com>
2025-11-07 22:34:12 +0530

    Add files via upload

---
+++ to implement big of 2 no if else statement.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a&b values");
+	scanf("%d%d",&a,&b);
+	if (a>b)
+	{
+		printf("%d is largest",a);
+	}
+	else
+	{
+		printf("%d is largest",b);
+	}
+	return 0;
+}
