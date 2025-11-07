4a2dd8b9849d960fd1e96b364d609b571dcd59c8
chanasyed4444-lang <chanasyed4444@gmail.com>
2025-11-07 22:18:59 +0530

    Add files via upload

---
+++ celsius to fahrenheit.c
@@ -0,0 +1,11 @@
+//celsius to fahrenheit
+#include<stdio.h>
+int main()
+{
+	float c,f;
+	printf("enter celsius value");
+	scanf("%f",&c);
+	f=(1.8*c)+32;
+	printf("fahrenheit value=%f",f);
+	return 0;
+}
