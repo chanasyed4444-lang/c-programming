4d91fc7b617f4a0f235086debf3b12d9fdbe9156
chanasyed4444-lang <chanasyed4444@gmail.com>
2025-11-07 22:42:15 +0530

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("enter a&b values");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	{
+		printf("%d is small",a);
+	}
+	else
+	{
+		printf("%d is small",b);
+	}
+	return 0;
+}
