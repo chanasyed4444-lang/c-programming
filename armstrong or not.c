95a285d2fd37bc7067d605434085dbbd2b1e6ebe
chanasyed4444-lang <chanasyed4444@gmail.com>
2025-11-08 10:36:17 +0530

    Add files via upload

---
+++ armstrong or not.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int n,d,c,sum=0,temp;
+	printf("enter n value");
+	scanf("%d",&n);
+	temp=n;
+	while(n>0)
+	{
+		d=n%10;
+		c=d*d*d;
+		sum=sum+c;
+		n=n/10;
+	}
+	if(sum==temp)
+	{
+		printf("it is armstrong");
+	}
+	else
+	printf("it is not armstrong");
+	return 0;
+}
